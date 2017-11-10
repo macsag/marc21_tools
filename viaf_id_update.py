0bd67c4ee4cb8ff75a8623ff024c32584f249354
macsag <maciejsagata@gmail.com>
2017-11-10 15:43:15 +0100

    v0.3

--- viaf_id_update.py
+++ viaf_id_update.py
@@ -3,14 +3,16 @@ from tqdm import tqdm
 from pymarc import *
 
 
-def load_and_select_from_viaf(viaf_input_file, data_dump=False):
+def load_and_select_from_viaf(viaf_input_file, siglum='NLP', data_dump=False):
+    # selects ids from viaf; for NLP use 'NLP' (default) / for NUKAT use 'NUKAT|'
 
     selected_viaf_ids = []
 
     with open(viaf_input_file, encoding='utf-8') as fp_in:
-            for line in tqdm(fp_in, total=81209613, desc='Przetwarzam plik viaf'):
+            for line in tqdm(fp_in, desc='Przetwarzam plik viaf'):
                 viaf, local = line.split('\t')
-                if 'NLP' in local[:3]:
+                length = len(siglum)
+                if siglum in local[:length]:
                     selected_viaf_ids.append((local.split('|')[1][:-1], viaf))
 
     if data_dump:
@@ -19,33 +21,44 @@ def load_and_select_from_viaf(viaf_input_file, data_dump=False):
     return selected_viaf_ids
 
 
-def load_and_select_from_catalogue(catalogue_input_file, data_dump=False):
+def load_and_select_from_catalogue(catalogue_input_file):
+    # creates 2 dictionaries: with all local ids and selected local ids
 
     selected_local_ids = {}
+    all_ids = {}
 
     with open(catalogue_input_file, 'rb') as fp:
-        reader = MARCReader(fp, to_unicode=True, force_utf8=True, utf8_handling='ignore')
-        for record in tqdm(reader, total=2200000, desc='Pobieram dane z katalogu'):
-            if record.get_fields('024'):
-                selected_local_ids[record.get_fields('001')[0].value()] = record.get_fields('024')[0].value()[:-4]
+        rdr = MARCReader(fp, to_unicode=True, force_utf8=True, utf8_handling='ignore')
+        for rcd in tqdm(rdr, total=2200000, desc='Pobieram dane z katalogu'):
+            try:
+                loc_id = rcd.get_fields('001')[0].value()
+            except IndexError:
+                continue
+            if rcd.get_fields('024'):
+                selected_local_ids[loc_id] = rcd.get_fields('024')[0].value()[:-4]
+                all_ids[loc_id] = None
+            else:
+                all_ids[loc_id] = None
 
-    if data_dump:
-        #todo
-        dump_to_tsv(selected_local_ids, 'viup_selected_local.tsv')
+    result = [all_ids, selected_local_ids]
 
-    return selected_local_ids
+    return result
 
 
-def catalogue_lookup(list_to_check, catalogue):
+def catalogue_lookup(list_to_check, all_ids, selected_ids, out_file):
 
     list_to_update = []
+    list_of_invalid_ids = []
 
-    for item in tqdm(list_to_check, desc='Sprawdzam w API'):
-        if item[0] not in catalogue:
-            item = ('001  ' + item[0], '024 7' + item[1] + '|2viaf', '996  .' + item[0])
+    for item in tqdm(list_to_check, desc='Sprawdzam w katalogu'):
+        if item[0] not in all_ids:
+            list_of_invalid_ids.append(item)
+        elif item[0] not in selected_ids:
+            item = ('001  ' + item[0], '0247 |a' + item[1] + '|2viaf', '996  |a.' + item[0])
             list_to_update.append(item)
 
-    dump_to_tsv(list_to_update, 'viup_to_update.tsv')
+    dump_to_tsv(list_to_update, out_file)
+    #dump_to_tsv(list_of_invalid_ids, 'viup_invalid_09102017.tsv')
 
 
 def dump_to_tsv(data_to_dump, output_file):
@@ -55,19 +68,12 @@ def dump_to_tsv(data_to_dump, output_file):
         w.writerows(data_to_dump)
 
 
-def make_marc_file(data):
+if __name__ == '__main__':
 
-    with open('viaf_to_update', 'rb') as fp:
-        reader = MARCReader(fp)
-        for record in reader:
-                record.add_ordered_field(
-                    Field(tag='046', indicators=[' ', ' '], subfields=['f', date_of_birth, 'g', date_of_death]))
-                out = open(fnameout, 'ab')
-                out.write(record.as_marc())
-                out.close()
-    #todo
+    viaf_file = 'viaf-20171001-links.txt'
+    loc_file = 'authorities-all.marc'
+    out = 'viup_to_update_09102017.tsv'
 
-
-loaded_from_viaf = load_and_select_from_viaf('viaf-20170806-links.txt')
-loaded_from_catalogue = load_and_select_from_catalogue('authorities-all.marc')
-catalogue_lookup(loaded_from_viaf, loaded_from_catalogue)
+    loaded_from_viaf = load_and_select_from_viaf(viaf_file)
+    loaded_from_catalogue = load_and_select_from_catalogue(loc_file)
+    catalogue_lookup(loaded_from_viaf, loaded_from_catalogue[0], loaded_from_catalogue[1])
